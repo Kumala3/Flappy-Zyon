bf78aa37cbf30dc5f03de7c5fc7ab45508f846ca
Kostek <ksytnik73kwer@gmail.com>
2024-09-27 23:41:34 +0200

    Add high_score.py for loading and saving high scores

---
+++ utils/high_score.py
@@ -0,0 +1,24 @@
+"""
+high_score.py
+
+Handles loading and saving the high score.
+"""
+
+
+def load_high_score():
+    """
+    Load the high score from a file.
+    """
+    try:
+        with open("high_score.txt", "r") as file:
+            return int(file.read())
+    except FileNotFoundError:
+        return 0
+
+
+def save_high_score(score):
+    """
+    Save the high score to a file.
+    """
+    with open("high_score.txt", "w") as file:
+        file.write(str(score))
